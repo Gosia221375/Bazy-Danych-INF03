d587cce835f6c106b1d1ce8eae07abc8a7f29c3c
Gosia221375 <100948927+Gosia221375@users.noreply.github.com>
2025-04-13 12:42:32 +0200

    Add files via upload

---
+++ Gosia/C++/Zajęcia 1/cwiczenia.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+int main() {
+	int a=2;
+    int b=4;
+
+	c=a+b;
+	c=c+1;
+	c=c++, c--;
+	//albo c=++c
+	c=b*c;
+
+	cout>>a //drukuje zawartość zmiennej c
+	cin>>b //wprowadza wartość zmiennej z klawiatury
+
+
+    bool x; //wyrażenie spełnione lub nie (prawda, fałsz)
+    x = (5==4); //wyjdzie false
+
+    if (x) {
+
+    }
+
+    for(int i=0; i<10; i++) {
+
+    } //pętla for ma 3 parametry (wartość początkowa (zmienna sterująca), warunek, zmiana wartości sterującej)
+}
---
+++ Gosia/C++/Zajęcia 1/przeliczanie.cpp
@@ -0,0 +1,9 @@
+//#include <iostream>
+
+int main() {
+    int x=137;
+
+    //cin>>a;
+
+    return 0;
+}
